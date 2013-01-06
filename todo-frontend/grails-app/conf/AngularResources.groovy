28075a8c6c1f130a16bc8eadf1ddec658e0d6cf8
Peter Schneider-Manzell <peter@schneider-manzell.de>
2013-01-06 15:28:52 +0100

    Added angular ui for a security question dialog before deleting a todo

--- todo-frontend/grails-app/conf/AngularResources.groovy
+++ todo-frontend/grails-app/conf/AngularResources.groovy
@@ -4,8 +4,14 @@ modules = {
         resource url:'http://ajax.googleapis.com/ajax/libs/angularjs/1.0.3/angular-resource.js',disposition: 'head'
     }
 
+    angularui {
+        dependsOn 'angular'
+        resource url:'js/angular-ui/0_3_2/angular-ui.css'
+        resource url:'js/angular-ui/0_3_2/angular-ui.js',disposition: 'head'
+    }
+
     todoapp {
-        dependsOn 'bootstrap,angular'
+        dependsOn 'bootstrap, angularui'
         resource url:'js/services.js',disposition: 'head'
         resource url:'js/controllers.js',disposition: 'head'
         resource url:'js/app.js',disposition: 'head'
